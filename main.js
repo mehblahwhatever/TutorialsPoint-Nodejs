33a89989ddec51f0de00b397e8fa27cfbc5ff790
Phillip <mehblahwhatever@gmail.com>
2016-09-19 11:51:45 -0400

    blocking code example

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-var http = require("http");
+var fs = require("fs");
 
-http.createServer(function (request, response) {
+var data = fs.readFileSync('input.txt');
 
-	// Send the HTTP header
-	// HTTP Status: 200 : OK
-	// Content Type: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-
-	// Send the response body as "Hello World"
-	response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log(data.toString());
+console.log("Program Ended");
