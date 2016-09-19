9286a84967cbf0b79c0d05a891d0ea65ff89cf3a
Phillip <mehblahwhatever@gmail.com>
2016-09-19 12:08:16 -0400

    Event Emitter example

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-var fs = require("fs");
+// Import events module
+var events = require('events');
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
 
-fs.readFile('input.txt', function (err, data) {
-	if(err) return console.error(err);
-	console.log(data.toString());
+// Create an event handler as follows
+var connectHandler = function connected() {
+	console.log('connection successful.');
+
+	// Fire the data_recieved event
+	eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+	console.log('data received successfully.');
 });
 
-console.log("Program Ended");
+// Fire the connection event
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
